361da9a258b62ca443603cadb917b649eeb2eb52
Amir Ossanloo <amir@amirossanloo.com>
2024-03-25 23:36:36 +0100

    Remove unused import

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
-import jwt, { type JwtPayload } from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import { path, pathOr } from 'ramda'
 import config from '../config'
 import type { EnrichedRequest } from '../typings/request'
