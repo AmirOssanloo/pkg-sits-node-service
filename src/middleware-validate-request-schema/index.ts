88fd8eae14aa6e1c81550d9f332dc1c6b4011194
Amir Ossanloo <amir@amirossanloo.com>
2024-12-25 16:49:01 +0100

    Update validate middleware

--- src/middleware-validate-request-schema/index.ts
+++ src/middleware-validate-request-schema/index.ts
@@ -1,17 +1,49 @@
 import { Response, NextFunction } from 'express'
-import Joi, { type Schema } from 'joi'
+import { type Schema, ValidationError as JoiValidationError } from 'joi'
 import { ValidationError } from '../errors'
 import { EnrichedRequest } from '../typings/request'
 
+interface ValidationSchema {
+  body?: Schema
+  query?: Schema
+  params?: Schema
+}
+
 const validateRequestSchemaMiddleware =
-  (schema: Schema) => (req: EnrichedRequest, res: Response, next: NextFunction) => {
-    const { error } = Joi.object(schema).validate({ query: req.query })
+  (schemas: ValidationSchema) => (req: EnrichedRequest, res: Response, next: NextFunction) => {
+    try {
+      if (schemas.body) {
+        const { error } = schemas.body.validate(req.body, { abortEarly: false })
 
-    if (error) {
-      throw new ValidationError(error.details[0].message)
+        if (error) {
+          throw error
+        }
+      }
+
+      if (schemas.query) {
+        const { error } = schemas.query.validate(req.query, { abortEarly: false })
+
+        if (error) {
+          throw error
+        }
+      }
+
+      if (schemas.params) {
+        const { error } = schemas.params.validate(req.params, { abortEarly: false })
+
+        if (error) {
+          throw error
+        }
+      }
+
+      next()
+    } catch (error) {
+      if (error instanceof JoiValidationError) {
+        throw new ValidationError(error.details[0].message)
+      }
+
+      throw error
     }
-
-    next()
   }
 
 export default validateRequestSchemaMiddleware
